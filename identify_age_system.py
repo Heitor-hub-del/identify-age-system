7f73780513bba7f620ccd9b338f86a10f7819711
Heitor-hub-del <heitortrader2023@gmail.com>
2025-07-17 19:18:06 -0300

    Add files via upload

---
+++ identify_age_system.py
@@ -0,0 +1,6 @@
+eleitor = int(input("Bem vindo as eleições!, para continuar vamos identificar a sua idade, digite sua idade: "))
+
+if eleitor >= 18:
+    print("você tem a idade necessária para votar!")
+else:
+    print("você não tem a idade necessária para votar!")
